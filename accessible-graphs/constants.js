acd375150a2614ed442c6b8b0afb485b4e776eba
y-yujun <y.yujun178@gmail.com>
2022-09-12 20:58:33 -0500

    Get svg from html

--- accessible-graphs/constants.js
+++ accessible-graphs/constants.js
@@ -6,8 +6,9 @@
 // const x_axes = ["GRID.text.70.1.1.tspan.1", "GRID.text.70.1.2.tspan.1", "GRID.text.70.1.3.tspan.1", "GRID.text.70.1.4.tspan.1", "GRID.text.70.1.5.tspan.1", "GRID.text.70.1.6.tspan.1", "GRID.text.70.1.7.tspan.1"];
 
 // barplot.diamonds_gridSVG.svg
-const bars = ["geom_rect.rect.2.1.1", "geom_rect.rect.2.1.2", "geom_rect.rect.2.1.3", "geom_rect.rect.2.1.4", "geom_rect.rect.2.1.5"]
-const x_axes = ["GRID.text.16.1.1.text", "GRID.text.16.1.2.text", "GRID.text.16.1.3.text", "GRID.text.16.1.4.text", "GRID.text.16.1.5.text"]
+const svg = document.getElementById("svg");
+const bars = ["geom_rect.rect.2.1.1", "geom_rect.rect.2.1.2", "geom_rect.rect.2.1.3", "geom_rect.rect.2.1.4", "geom_rect.rect.2.1.5"];
+const x_axes = ["GRID.text.16.1.1.text", "GRID.text.16.1.2.text", "GRID.text.16.1.3.text", "GRID.text.16.1.4.text", "GRID.text.16.1.5.text"];
 
 const height = bars.map(getHeight);
 const x_values = x_axes.map(getX);
@@ -33,8 +34,8 @@ const range = (ymax - ymin) / 4;
 
 function getHeight(item) {
     return document.getElementById(item).getAttribute('height');
-  }
+}
   
 function getX(item) {
-return document.getElementById(item).innerHTML;
+    return document.getElementById(item).innerHTML;
 }
