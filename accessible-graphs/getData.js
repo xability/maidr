02fe7302a707b10dcdbf50e61a1104ad165aeb50
y-yujun <y.yujun178@gmail.com>
2022-09-14 12:42:06 -0500

    Get data from json format

--- accessible-graphs/getData.js
+++ accessible-graphs/getData.js
@@ -7,13 +7,33 @@ var yVal;
 form.addEventListener("submit", function(e) {
     e.preventDefault();
     let input = document.getElementById("yVal").value;
-    yVal = input.split(",").map(function(item) {
-        return parseFloat(item.trim());
-    });
+    var inputObject = parseJSONObject(input);
+    xVal = []
+    yVal = []
+    for (var key in inputObject) {
+        if (inputObject.hasOwnProperty(key)) {
+            xVal.push(key);
+            yVal.push(inputObject[key]);
+        }
+    }
+    // yVal = input.split(",").map(function(item) {
+    //     return parseFloat(item.trim());
+    // });
+    sessionStorage.setItem("xVal", xVal);
     sessionStorage.setItem("yVal", yVal);
     if (yVal.length != _numBars) {
         invalid_text.innerHTML = "Invalid input";
     } else {
-        window.location.assign("/Users/y.yujun/Documents/Data accessiblization/accessible-graphs (local)/barplot.html");
+        window.location.assign("/Users/y.yujun/Documents/Data accessiblization/accessible-graph/accessible-graphs/barplot.html");
     }
 })
+
+function parseJSONObject(json) {
+    try {
+        var obj = JSON.parse(json);
+        if (obj && typeof(obj) == "object") {
+            return obj;
+        }
+    } catch(e) {}
+    return false;
+}
