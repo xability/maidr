b63efe0bfcdba16788f8ce6f0c887130bc12dde3
JooYoung Seo <jseo1005@illinois.edu>
2022-09-20 08:53:08 -0500

    R Script update

--- accessible-graphs/ggplot_to_svg.R
+++ accessible-graphs/ggplot_to_svg.R
@@ -1,8 +1,20 @@
+# Load required packages
 library(gridSVG)
 library(ggplot2)
 
+# Bar plot sample
 ggplot(mpg, aes(class)) +
     geom_bar()
 
+
 gridSVG::grid.export("barplot.svg")
+dev.off()
+
+
+# Box plot sample
+ggplot(data = mpg, mapping = aes(x = class, y = hwy)) +
+    geom_boxplot() +
+    coord_flip()
+
+gridSVG::grid.export("boxplot.svg")
 dev.off()
