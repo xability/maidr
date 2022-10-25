85727b6a7e945980d97d536294693870ab8d66ff
ellvix <sean.m.mccurry@gmail.com>
2022-10-25 14:33:07 -0600

    finished merge conflicts

--- accessible-graphs/constants.js
+++ accessible-graphs/constants.js
@@ -23,45 +23,6 @@ class Constants {
     debugLevel = 5; // 0 = no console output, 1 = some console, 2 = more console, etc
 }
 
-<<<<<<< HEAD
-class Position {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-=======
-// temp use so that barplot still works
-const bars = ["geom_rect.rect.2.1.1", "geom_rect.rect.2.1.2", "geom_rect.rect.2.1.3", "geom_rect.rect.2.1.4", "geom_rect.rect.2.1.5"];
-const x_axes = ["GRID.text.16.1.1.text", "GRID.text.16.1.2.text", "GRID.text.16.1.3.text", "GRID.text.16.1.4.text", "GRID.text.16.1.5.text"];
-
-// bookmark: setting this up to pull from various sources. Gotta figure out what we need for boxplot and seperate it out
-
-const height = bars.map(getHeight);
-const extracted_x_values = x_axes.map(getX);
-
-var x_val = sessionStorage.getItem("xVal");
-var y_val = sessionStorage.getItem("yVal");
-var x_values;
-var y_values;
-var ymin;
-var ymax;
-
-if (y_val != null) {
-    x_values = x_val.split(",");
-    y_values = y_val.split(",").map(str => {
-        return parseFloat(str);
-    });
-    ymin = Math.min(...y_values);
-    ymax = Math.max(...y_values);
-} else {
-    // if we open this file before asking for y values
-    x_values = extracted_x_values;
-    y_values = height;
-    ymin = Math.min(...height);
-    ymax = Math.max(...height);
->>>>>>> main
-}
-
 // HELPER FUNCTIONS
 class Helper {
 
