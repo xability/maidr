ee79a7eec102b119df1c3d009ac90bccdbc8a904
Yu Jun <yujuny2@github.com>
2022-11-09 22:32:17 -0600

    Scatterplot implementation

---
+++ scatterplot.js
@@ -0,0 +1,13 @@
+import prediction_array from './scatterplot/prediction_array' assert { type: 'json' };
+import residual_array from './scatterplot/residual_array' assert { type: 'json' };
+
+// class ScatterPlot {
+//     constructor() {
+//         this.plots = document.querySelectorAll('#' + constants.plotId.replaceAll('\.', '\\.') + ' > use');
+//         this.plotData = this.getPoints();
+//     }
+
+//     getPoints() {
+
+//     }
+// }
--- scatterplot/scatterplot-constants.js
+++
@@ -1,4 +0,0 @@
-const svg = document.getElementById("scatterplot-svg");
-const points = document.querySelector("[id^='geom_point.points.2.1.']");
-
-// create points array
