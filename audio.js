06fec7136f057f87b4d96cd20689b49d6bb18ae4
Yu Jun <yujuny2@github.com>
2022-11-12 21:57:02 -0600

    Fixed audio

--- audio.js
+++ audio.js
@@ -84,7 +84,6 @@ class Audio {
             }
         } else if (constants.chartType == "heatmap") {    // Added heatmap tone feature
             if (rawFreq != 0) {
-                console.log(rawFreq);
                 this.playOscillator(rawFreq, currentDuration, panning, constants.vol, 'sine');
             } else {
                 this.playOscillator(frequency, currentDuration, panning, constants.vol / 2, 'square');
