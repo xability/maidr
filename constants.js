e3adf4e14d2859741d6d96a27cff3512c3d5a24b
ellvix <sean.m.mccurry@gmail.com>
2023-01-18 20:41:04 -0700

    updated tracker, we now log aria-live text

--- constants.js
+++ constants.js
@@ -378,9 +378,6 @@ class Tracker {
         if (!(constants.minY === undefined || constants.minY === null)) {
             eventToLog.minY = Object.assign(constants.minY);
         }
-        if (!(constants.chartType === undefined || constants.chartType === null)) {
-            eventToLog.chartType = Object.assign(constants.chartType);
-        }
         if (!(constants.MAX_FREQUENCY === undefined || constants.MAX_FREQUENCY === null)) {
             eventToLog.MAX_FREQUENCY = Object.assign(constants.MAX_FREQUENCY);
         }
@@ -429,6 +426,14 @@ class Tracker {
         if (!(constants.layer === undefined || constants.layer === null)) {
             eventToLog.scatterplotLayer = Object.assign(constants.layer);
         }
+        if (!(constants.chartType === undefined || constants.chartType === null)) {
+            eventToLog.chartType = Object.assign(constants.chartType);
+        }
+        if (!(constants.infoDiv.innerHTML === undefined || constants.infoDiv.innerHTML === null)) {
+            eventToLog.textDisplay = Object.assign(constants.infoDiv.innerHTML);
+        }
+        
+        console.log(eventToLog);
 
         this.data.events.push(eventToLog);
 
