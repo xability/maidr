be91a4eeb1104672ebe535e14ab50fb47126398c
JooYoung Seo <jseo1005@illinois.edu>
2024-03-05 15:10:08 -0600

    ci(husky): update body length to 1000 and allow graceful warning

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'footer-max-line-length': [2, 'always', 1000],
+        'body-max-line-length': [1, 'always', 1000],
+    'footer-max-line-length': [2, 'always', 1000]    
   },
 };
