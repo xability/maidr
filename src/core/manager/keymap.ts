9689489f5ead3c5a7ca0327e4c04703c4e5727bf
Saairam Venkatesh <greenghost1100@gmail.com>
2024-10-28 20:15:55 -0400

    feat: add scoping hotkeys (#43)

---
+++ src/core/manager/keymap.ts
@@ -0,0 +1,83 @@
+import hotkeys from 'hotkeys-js';
+import Constant from '../../util/constant';
+import {CommandFactory} from '../command/factory';
+import {CommandContext} from '../command/command';
+
+export enum DefaultKey {
+  ACTIVATE_LABEL_SCOPE = 'l',
+
+  // Navigation
+  MOVE_UP = 'up',
+  MOVE_DOWN = 'down',
+  MOVE_RIGHT = 'right',
+  MOVE_LEFT = 'left',
+
+  // BTS
+  TOGGLE_BRAILLE = 'b',
+  TOGGLE_TEXT = 't',
+  TOGGLE_AUDIO = 's',
+}
+
+export enum LabelKey {
+  ACTIVATE_DEFAULT_SCOPE = 'esc',
+
+  // Description
+  DESCRIBE_X = 'x',
+  DESCRIBE_Y = 'y',
+  DESCRIBE_POINT = 'space',
+}
+
+const scopedKeymap = {
+  DEFAULT: DefaultKey,
+  LABEL: LabelKey,
+};
+
+export type Scope = keyof typeof scopedKeymap;
+export type Keymap = {
+  [K in Scope]: (typeof scopedKeymap)[K];
+};
+export type Keys = keyof Keymap[Scope];
+
+export default class KeymapManager {
+  private readonly commandFactory: CommandFactory;
+  constructor(commandContext: CommandContext) {
+    this.commandFactory = new CommandFactory(commandContext);
+  }
+
+  public register(): void {
+    hotkeys.filter = (event: KeyboardEvent) => {
+      const target = event.target as HTMLElement;
+      // Allow keybindings only for MAIDR braille input.
+      if (target.isContentEditable) {
+        return target.id.startsWith(Constant.BRAILLE_INPUT);
+      }
+
+      // Allow keybindings for all other non-editable elements.
+      return true;
+    };
+
+    // Register all bindings.
+    for (const [scope, keymap] of Object.entries(scopedKeymap) as [
+      Scope,
+      Keymap[Scope],
+    ][]) {
+      for (const [commandName, key] of Object.entries(keymap) as [
+        Keys,
+        string,
+      ][]) {
+        const command = this.commandFactory.create(commandName);
+        hotkeys(key, {scope: scope}, (event: KeyboardEvent): void => {
+          event.preventDefault();
+          command.execute(event);
+        });
+      }
+    }
+
+    // Set the initial scope.
+    hotkeys.setScope('DEFAULT');
+  }
+
+  public unregister(): void {
+    hotkeys.unbind();
+  }
+}
--- src/util/constant.ts
+++ src/util/constant.ts
@@ -1,19 +1,21 @@
 export default abstract class Constant {
   // HTML elements.
+  static readonly ARTICLE = 'article';
   static readonly BR = 'br';
   static readonly DIV = 'div';
+  static readonly FIGURE = 'figure';
   static readonly INPUT = 'input';
   static readonly P = 'p';
 
   // HTML IDs.
-  static readonly BRAILLE_CONTAINER_ID = 'maidr-braille-container';
-  static readonly BRAILLE_INPUT_ID = 'maidr-braille-input';
+  static readonly BRAILLE_CONTAINER = 'maidr-braille-container-';
+  static readonly BRAILLE_INPUT = 'maidr-braille-input-';
   static readonly BRAILLE_INPUT_LENGTH = 32;
-  static readonly MAIN_CONTAINER_ID = 'maidr-main-container';
-  static readonly NOTIFICATION_CONTAINER_ID = 'maidr-notification-container';
-  static readonly PLOT_CONTAINER_ID = 'maidr-plot-container';
-  static readonly TEXT_CONTAINER_ID = 'maidr-info-container';
-  static readonly REVIEW_CONTAINER_ID = 'maidr-review-container';
+  static readonly MAIDR_ARTICLE = 'maidr-article-';
+  static readonly MAIDR_BR = 'maidr-br-';
+  static readonly MAIDR_FIGURE = 'maidr-figure-';
+  static readonly NOTIFICATION_CONTAINER = 'maidr-notification-container-';
+  static readonly TEXT_CONTAINER = 'maidr-info-container-';
 
   // HTML Classes.
   static readonly BRAILLE_INPUT_CLASS = 'maidr-braille-input';
@@ -42,4 +44,8 @@ export default abstract class Constant {
   static readonly SPACE = ' ';
   static readonly COMMA = ', ';
   static readonly THROUGH = ' through ';
+
+  // Hotkeys Scopes
+  static readonly HOTKEYS_SCOPE_DEFAULT = 'all';
+  static readonly HOTKEYS_SCOPE_LABEL = 'label';
 }
