e3cad14704e16e21de7b8d2d6ec9758ecb06afdc
Krishna Anandan Ganesan <krishna1729atom@gmail.com>
2025-02-24 16:30:49 -0600

    test: integrate lineplot tests and refactor movement based test functions (#99)

--- cypress/e2e/plot/test_lineplot.cy.ts
+++ cypress/e2e/plot/test_lineplot.cy.ts
@@ -3,12 +3,12 @@ import { TestConstants } from '../../util/constant';
 import * as helper from '../../util/helper';
 import 'cypress-real-events/support';
 
-describe('Bar Plot', () => {
+describe('Lineplot', () => {
   let maidrData: Maidr;
 
   before(() => {
     // Visit the file and extract the maidr data before all tests
-    cy.visit('examples/barplot.html');
+    cy.visit('examples/lineplot.html');
     cy.window().then((window) => {
       maidrData = window.maidr;
     });
@@ -16,88 +16,88 @@ describe('Bar Plot', () => {
 
   beforeEach(() => {
     // Visit the file before each test
-    cy.visit('examples/barplot.html');
+    cy.visit('examples/lineplot.html');
   });
 
-  it('should load the barplot with maidr data', () => {
-    helper.verifyPlotLoaded(TestConstants.BAR_ID);
+  it('should load the lineplot with maidr data', () => {
+    helper.verifyPlotLoaded(TestConstants.LINE_ID);
   });
 
   it('should activate maidr on click', () => {
-    helper.verifyMaidrActivated(TestConstants.BAR_ID);
+    helper.verifyMaidrActivated(TestConstants.LINE_ID);
   });
 
   it('should display instruction text', () => {
-    cy.get(TestConstants.HASH + TestConstants.BAR_ID).click();
-    cy.get(`#${TestConstants.MAIDR_NOTIFICATION_CONTAINER + TestConstants.BAR_ID} ${TestConstants.PARAGRAPH}`)
+    cy.get(TestConstants.HASH + TestConstants.LINE_ID).click();
+    cy.get(`#${TestConstants.MAIDR_NOTIFICATION_CONTAINER + TestConstants.LINE_ID} ${TestConstants.PARAGRAPH}`)
       .invoke('text')
       .then((text) => {
         const normalizedText = text.replace(/\s+/g, ' ').trim();
-        expect(normalizedText).to.equal(TestConstants.BAR_INSTRUCTION_TEXT);
+        expect(normalizedText).to.equal(TestConstants.LINE_INSTRUCTION_TEXT);
       });
   });
 
   it('should be able to move from left to right', () => {
-    helper.verifyHorizontalMovement(maidrData, TestConstants.BAR_ID, TestConstants.HORIZONTAL_FORWARD);
+    helper.verifyHorizontalMovement(maidrData, TestConstants.LINE_ID, TestConstants.HORIZONTAL_FORWARD);
   });
 
   it('should be able to move from right to left', () => {
     // Move to right extreme point
     helper.verifyExtremePoint(TestConstants.LINE_ID, TestConstants.RIGHT);
-    helper.verifyHorizontalMovement(maidrData, TestConstants.BAR_ID, TestConstants.HORIZONTAL_REVERSE);
+    helper.verifyHorizontalMovement(maidrData, TestConstants.LINE_ID, TestConstants.HORIZONTAL_REVERSE);
   });
 
   it('toggle text mode', () => {
-    helper.verifyToggleTextMode(TestConstants.BAR_ID);
+    helper.verifyToggleTextMode(TestConstants.LINE_ID);
   });
 
   it('toggle braille mode', () => {
-    helper.verifyToggleBrailleMode(TestConstants.BAR_ID);
+    helper.verifyToggleBrailleMode(TestConstants.LINE_ID);
   });
 
   it('toggle sonification', () => {
-    helper.verifyToggleSonification(TestConstants.BAR_ID);
+    helper.verifyToggleSonification(TestConstants.LINE_ID);
   });
 
   it('should be able to increase speed', () => {
-    helper.verifyIncreaseSpeed(TestConstants.BAR_ID);
+    helper.verifyIncreaseSpeed(TestConstants.LINE_ID);
   });
 
   it('should be able to decrease speed', () => {
-    helper.verifyDecreaseSpeed(TestConstants.BAR_ID);
+    helper.verifyDecreaseSpeed(TestConstants.LINE_ID);
   });
 
   it('should be able to reset speed', () => {
-    helper.verifyResetSpeed(TestConstants.BAR_ID);
+    helper.verifyResetSpeed(TestConstants.LINE_ID);
   });
 
   it('should play left extreme point', () => {
-    helper.verifyExtremePoint(TestConstants.BAR_ID, TestConstants.LEFT);
+    helper.verifyExtremePoint(TestConstants.LINE_ID, TestConstants.LEFT);
   });
 
   it('should play right extreme point', () => {
-    helper.verifyExtremePoint(TestConstants.BAR_ID, TestConstants.RIGHT);
+    helper.verifyExtremePoint(TestConstants.LINE_ID, TestConstants.RIGHT);
   });
 
   it('should replay the same point', () => {
-    helper.verifyReplaySamePoint(TestConstants.BAR_ID);
+    helper.verifyReplaySamePoint(TestConstants.LINE_ID);
   });
 
   it('Braille Navigation - left to right', () => {
-    helper.verifyBrailleNavigationForward(maidrData, TestConstants.BAR_ID);
+    helper.verifyBrailleNavigationForward(maidrData, TestConstants.LINE_ID);
   });
 
   it('Braille Navigation - right to left', () => {
     helper.verifyExtremePoint(TestConstants.LINE_ID, TestConstants.RIGHT);
-    helper.verifyBrailleNavigationReverse(maidrData, TestConstants.BAR_ID);
+    helper.verifyBrailleNavigationReverse(maidrData, TestConstants.LINE_ID);
   });
 
   it('Autoplay - left to right', () => {
-    helper.verifyAutoplay(maidrData, TestConstants.BAR_ID, TestConstants.HORIZONTAL_FORWARD);
+    helper.verifyAutoplay(maidrData, TestConstants.LINE_ID, TestConstants.HORIZONTAL_FORWARD);
   });
 
   it('Autoplay - right to left', () => {
     helper.verifyExtremePoint(TestConstants.LINE_ID, TestConstants.RIGHT);
-    helper.verifyAutoplay(maidrData, TestConstants.BAR_ID, TestConstants.HORIZONTAL_REVERSE);
+    helper.verifyAutoplay(maidrData, TestConstants.LINE_ID, TestConstants.HORIZONTAL_REVERSE);
   });
 });
