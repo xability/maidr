c6cda26f3e6fd044007f8c5fdbfb7a45181e9cbb
SaaiVenkat <greenghost1100@gmail.com>
2025-02-24 12:17:29 -0600

    chore: move selector to app selector

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,13 +1,13 @@
-import type { RootState } from '@redux/store';
+import { useAppSelector } from '@redux/hook/useStore';
 import { store } from '@redux/store';
 import Help from '@ui/pages/Help';
 import Settings from '@ui/pages/Settings';
 import React from 'react';
-import { Provider, useSelector } from 'react-redux';
+import { Provider } from 'react-redux';
 
 const App: React.FC = () => {
-  const { enabled: isHelpEnabled } = useSelector((state: RootState) => state.helpMenu);
-  const { enabled: isSettingsEnabled } = useSelector((state: RootState) => state.settings);
+  const { enabled: isHelpEnabled } = useAppSelector(state => state.helpMenu);
+  const { enabled: isSettingsEnabled } = useAppSelector(state => state.settings);
 
   return (
     <>
