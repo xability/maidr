c832033c7ef583d6adadc56920658641ff0d3b3e
ellvix <sean.m.mccurry@gmail.com>
2025-02-24 19:58:34 -0700

    feat: highlighting for bar and hist charts

--- src/model/plot.ts
+++ src/model/plot.ts
@@ -1,6 +1,12 @@
 import type { BarPoint, Maidr } from './grammar';
 import type { Movable, Observable, Observer } from './interface';
-import type { AudioState, AutoplayState, BrailleState, PlotState, TextState } from './state';
+import type {
+  AudioState,
+  AutoplayState,
+  BrailleState,
+  PlotState,
+  TextState,
+} from './state';
 import { MovableDirection } from './interface';
 
 const DEFAULT_TITLE = 'MAIDR Plot';
@@ -57,6 +63,8 @@ export abstract class AbstractPlot<T> implements Plot {
   public readonly yAxis: string;
   protected readonly fill: string;
 
+  public readonly selector: string;
+
   protected values: T[][];
   protected brailleValues: string[][];
 
@@ -65,7 +73,6 @@ export abstract class AbstractPlot<T> implements Plot {
 
   protected constructor(maidr: Maidr) {
     this.observers = [];
-    this.isOutOfBounds = false;
 
     this.id = maidr.id;
     this.type = maidr.type;
@@ -78,11 +85,13 @@ export abstract class AbstractPlot<T> implements Plot {
     this.yAxis = maidr.axes?.y ?? DEFAULT_Y_AXIS;
     this.fill = maidr.axes?.fill ?? DEFAULT_FILL_AXIS;
 
+    this.selector = maidr.selector ?? '';
+
     this.values = [];
     this.brailleValues = [];
 
     this.isOutOfBounds = true;
-    this.row = 0;
+    this.row = -1;
     this.col = -1;
   }
 
@@ -104,7 +113,9 @@ export abstract class AbstractPlot<T> implements Plot {
   protected notifyOutOfBounds(): void {
     this.isOutOfBounds = true;
     this.notifyStateUpdate();
-    this.isOutOfBounds = false;
+    if (this.row < 0 && this.col < 0) {
+      this.isOutOfBounds = false;
+    }
   }
 
   protected braille(): BrailleState {
@@ -131,11 +142,37 @@ export abstract class AbstractPlot<T> implements Plot {
   }
 
   public moveOnce(direction: MovableDirection): void {
+    // we set our movement functions to account for our -1, -1 starting position
     const movement = {
-      UPWARD: () => (this.row += 1),
-      DOWNWARD: () => (this.row -= 1),
-      FORWARD: () => (this.col += 1),
-      BACKWARD: () => (this.col -= 1),
+      UPWARD: () => {
+        this.row += 1;
+        this.row = this.values.length - 1;
+        if (this.col < 0)
+          this.col = 0;
+        if (this.col > this.values[this.row]?.length - 1)
+          this.col = this.values[this.row].length - 1;
+      },
+      DOWNWARD: () => {
+        this.row -= 1;
+        if (this.col < 0)
+          this.col = 0;
+        if (this.col > this.values[this.row]?.length - 1)
+          this.col = this.values[this.row].length - 1;
+      },
+      FORWARD: () => {
+        this.col += 1;
+        if (this.row < 0)
+          this.row = 0;
+        if (this.row > this.values.length - 1)
+          this.row = this.values.length - 1;
+      },
+      BACKWARD: () => {
+        this.col -= 1;
+        if (this.row < 0)
+          this.row = 0;
+        if (this.row > this.values.length - 1)
+          this.row = this.values.length - 1;
+      },
     };
 
     if (this.isMovable(direction)) {
@@ -149,19 +186,47 @@ export abstract class AbstractPlot<T> implements Plot {
 
   public moveToExtreme(direction: MovableDirection): void {
     const movement = {
-      UPWARD: () => (this.row = 0),
-      DOWNWARD: () => (this.row = this.values.length - 1),
-      FORWARD: () => (this.col = this.values[this.row].length - 1),
-      BACKWARD: () => (this.col = 0),
+      UPWARD: () => {
+        this.row = this.values.length - 1;
+        if (this.col < 0)
+          this.col = 0;
+        if (this.col > this.values[this.row]?.length - 1)
+          this.col = this.values[this.row].length - 1;
+      },
+      DOWNWARD: () => {
+        this.row = 0;
+        if (this.col < 0)
+          this.col = 0;
+        if (this.col > this.values[this.row]?.length - 1)
+          this.col = this.values[this.row].length - 1;
+      },
+      FORWARD: () => {
+        if (this.row < 0)
+          this.row = 0;
+        if (this.row > this.values.length - 1)
+          this.row = this.values.length - 1;
+        this.col = this.values[this.row].length - 1;
+      },
+      BACKWARD: () => {
+        this.col = 0;
+        if (this.row < 0)
+          this.row = 0;
+        if (this.row > this.values.length - 1)
+          this.row = this.values.length - 1;
+      },
     };
 
+    this.isOutOfBounds = false;
     movement[direction]();
     this.notifyStateUpdate();
   }
 
   public moveToIndex(index: number): void {
     if (this.isMovable(index)) {
+      if (this.row < 0)
+        this.row = 0;
       this.col = index;
+      this.isOutOfBounds = false;
       this.notifyStateUpdate();
     }
   }
@@ -175,7 +240,12 @@ export abstract class AbstractPlot<T> implements Plot {
         return this.row > 0;
 
       case MovableDirection.FORWARD:
-        return this.col < this.values[this.row].length - 1;
+        // we start charts at -1-1, so we need to not break on the first move
+        return (
+          this.row === -1
+          || this.col === -1
+          || this.col < this.values[this.row].length - 1
+        );
 
       case MovableDirection.BACKWARD:
         return this.col > 0;
@@ -217,6 +287,12 @@ export abstract class AbstractBarPlot<
   protected readonly min: number[];
   protected readonly max: number[];
 
+  // visual highlighting vars
+  protected activeElement: HTMLElement | undefined;
+  protected activeElementColor: string | undefined;
+  protected readonly defaultElementColor: string = '#BADA55';
+  protected barElements: NodeListOf<HTMLElement> | undefined;
+
   protected constructor(maidr: Maidr, points: T[][]) {
     super(maidr);
 
@@ -234,6 +310,15 @@ export abstract class AbstractBarPlot<
     this.max = this.values.map(row => Math.max(...row));
 
     this.brailleValues = this.toBraille(this.values);
+
+    if (maidr.selector) {
+      this.barElements = document.querySelectorAll(this.selector);
+    }
+  }
+
+  public notifyStateUpdate(): void {
+    super.notifyStateUpdate();
+    this.Select();
   }
 
   protected audio(): AudioState {
@@ -301,4 +386,193 @@ export abstract class AbstractBarPlot<
 
     return braille;
   }
+
+  /**
+   * Function to convert hexadecimal color to string formatted rgb() functional notation.
+   * @param hexColorString - hexadecimal color (e.g., "#595959").
+   * @returns rgb() functional notation string (e.g., "rgb(100,100,100)").
+   */
+  ConvertHexToRGBString(hexColorString: string): string {
+    return `rgb(${Number.parseInt(
+      hexColorString.slice(1, 3),
+      16,
+    )},${Number.parseInt(hexColorString.slice(3, 5), 16)},${Number.parseInt(
+      hexColorString.slice(5, 7),
+      16,
+    )})`;
+  }
+
+  /**
+   * Function to convert an rgb() functional notation string to hexadecimal color.
+   * @param rgbColorString - color in rgb() functional notation (e.g., "rgb(100,100,100)").
+   * @returns hexadecimal color (e.g., "#595959").
+   */
+  ConvertRGBStringToHex(rgbColorString: string): string {
+    const rgb: number[] = rgbColorString
+      .replace(/[^\d,]/g, '')
+      .split(',')
+      .map(Number);
+    return `#${rgb[0].toString(16).padStart(2, '0')}${rgb[1]
+      .toString(16)
+      .padStart(2, '0')}${rgb[2].toString(16).padStart(2, '0')}`;
+  }
+
+  /**
+   * Inverts an RGB color by subtracting each color component from 255.
+   *
+   * @param color - The RGB color to invert, in the format "rgb(r,g,b)".
+   * @return The inverted RGB color, in the format "rgb(r,g,b)".
+   */
+  ColorInvert(color: string): string {
+    const rgb: number[] = color
+      .replace(/[^\d,]/g, '')
+      .split(',')
+      .map(Number);
+    const r: number = 255 - rgb[0];
+    const g: number = 255 - rgb[1];
+    const b: number = 255 - rgb[2];
+    return `rgb(${r},${g},${b})`;
+  }
+
+  /**
+   * Determines the best contrast color for the given color, by inverting it if necessary,
+   * but if it's just a shade of gray, default to this.colorSelected.
+   * @param oldColor - The color to make better
+   * @returns The better color
+   */
+  GetBetterColor(oldColor: string): string {
+    if (oldColor.includes('#')) {
+      oldColor = this.ConvertHexToRGBString(oldColor);
+    }
+    let newColor: string = this.ColorInvert(oldColor);
+    const rgb: number[] = newColor
+      .replace(/[^\d,]/g, '')
+      .split(',')
+      .map(Number);
+
+    if (
+      rgb.length === 3 // Ensure valid RGB values
+      && rgb[1] < rgb[0] + 10
+      && rgb[1] > rgb[0] - 10
+      && rgb[2] < rgb[0] + 10
+      && rgb[2] > rgb[0] - 10
+      && (rgb[0] > 86 || rgb[0] < 169)
+    ) {
+      // Too gray and too close to center gray, use default
+      newColor = this.defaultElementColor;
+    }
+
+    return newColor;
+  }
+
+  /**
+   * Function to parse a string containing CSS styles and return an array of strings containing CSS style attributes and values.
+   * @param styleString - a string containing CSS styles in inline format.
+   * @returns an array of strings containing CSS style attributes and values.
+   */
+  GetStyleArrayFromString(styleString: string): string[] {
+    // Get an array of CSS style attributes and values from a style string
+    return styleString.replaceAll(' ', '').split(/[:;]/);
+  }
+
+  /**
+   * Function to parse an array of strings containing CSS style attributes and values and return a string containing CSS styles.
+   * @param styleArray - an array of strings containing CSS style attributes and values.
+   * @returns a string containing the CSS styles.
+   */
+  GetStyleStringFromArray(styleArray: string[]): string {
+    // Get CSS style string from an array of style attributes and values
+    let styleString = '';
+    for (let i = 0; i < styleArray.length; i++) {
+      if (i % 2 === 0) {
+        if (i !== styleArray.length - 1) {
+          styleString += `${styleArray[i]}: `;
+        } else {
+          styleString += styleArray[i];
+        }
+      } else {
+        styleString += `${styleArray[i]}; `;
+      }
+    }
+    return styleString;
+  }
+
+  /**
+   * Selects the active element and changes its color.
+   */
+  Select(): void {
+    this.UnSelectPrevious();
+    if (this.barElements) {
+      this.activeElement = this.barElements[this.col] as
+      | HTMLElement
+      | undefined;
+      if (this.activeElement) {
+        // Case where fill is a direct attribute
+        if (this.activeElement.hasAttribute('fill')) {
+          this.activeElementColor
+            = this.activeElement.getAttribute('fill') || '';
+          // Get new color to highlight and replace fill value
+          this.activeElement.setAttribute(
+            'fill',
+            this.GetBetterColor(this.activeElementColor),
+          );
+        } else if (
+          // Case where fill is within the style attribute
+          this.activeElement.hasAttribute('style')
+          && this.activeElement.getAttribute('style')!.includes('fill')
+        ) {
+          let styleString = this.activeElement.getAttribute('style')!;
+          // Extract all style attributes and values
+          const styleArray = this.GetStyleArrayFromString(styleString);
+          const fillIndex = styleArray.indexOf('fill');
+
+          if (fillIndex !== -1 && fillIndex + 1 < styleArray.length) {
+            this.activeElementColor = styleArray[fillIndex + 1];
+            // Get new color to highlight and replace fill value in style array
+            styleArray[fillIndex + 1] = this.GetBetterColor(
+              this.activeElementColor,
+            );
+            // Recreate style string and set style attribute
+            styleString = this.GetStyleStringFromArray(styleArray);
+            this.activeElement.setAttribute('style', styleString);
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * Unselects the previously selected element by setting its fill attribute to the original color.
+   */
+  UnSelectPrevious(): void {
+    if (this.activeElement) {
+      // Set fill attribute to the original color
+      if (this.activeElement.hasAttribute('fill')) {
+        if (this.activeElementColor) {
+          this.activeElement.setAttribute('fill', this.activeElementColor);
+        }
+        this.activeElement = undefined;
+      } else if (
+        // Case where fill is within the style attribute
+        this.activeElement.hasAttribute('style')
+        && this.activeElement.getAttribute('style')!.includes('fill')
+      ) {
+        let styleString = this.activeElement.getAttribute('style')!;
+        const styleArray = this.GetStyleArrayFromString(styleString);
+        const fillIndex = styleArray.indexOf('fill');
+
+        if (fillIndex !== -1 && fillIndex + 1 < styleArray.length) {
+          if (this.activeElementColor) {
+            styleArray[fillIndex + 1] = this.activeElementColor;
+          }
+          // Recreate style string and set style attribute
+          styleString = this.GetStyleStringFromArray(styleArray);
+          this.activeElement.setAttribute('style', styleString);
+        }
+        this.activeElement = undefined;
+      }
+    }
+  }
 }
+
+// bookmark: just added all the above code, copied over from js, now it's time to run and debug
