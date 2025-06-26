f69dce5084b654d525243b687551906b17b7f938
nk46-cloud <nk46@illinois.edu>
2025-06-26 15:58:48 -0500

    fix: support multi-line axis label & titles (#370)
    * fix: address empty labels in candlestick plots
    
    * feat: add multi-line label & title

--- src/type/grammar.ts
+++ src/type/grammar.ts
@@ -71,6 +71,15 @@ export interface LinePoint {
   fill?: string;
 }
 
+export interface LineDataItem {
+  title?: string;
+  axes?: {
+    x?: string;
+    y?: string;
+  };
+  points: LinePoint[];
+}
+
 export interface ScatterPoint {
   x: number;
   y: number;
@@ -110,6 +119,7 @@ export interface MaidrLayer {
     | HeatmapData
     | HistogramPoint[]
     | LinePoint[][]
+    | LineDataItem[]
     | ScatterPoint[]
     | SegmentedPoint[][];
 }
