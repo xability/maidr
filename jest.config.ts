fa31a6f84af3da213d2fe78af4f320953162a55c
SaaiVenkat <greenghost1100@gmail.com>
2025-08-17 11:05:20 -0600

    fix: ci json import for latest node lts

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from '@jest/types';
 import { pathsToModuleNameMapper } from 'ts-jest';
-import { compilerOptions } from './tsconfig.json';
+import { compilerOptions } from './tsconfig.json' with { type: "json" };
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
