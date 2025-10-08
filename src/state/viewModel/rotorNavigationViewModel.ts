c13cf07029ffdfac8f34955bbd3ba7b1d3660032
aarthy2000 <aarthymaha2000@gmail.com>
2025-10-08 01:30:26 -0500

    chore: lint, fix copilot suggestion

--- src/state/viewModel/rotorNavigationViewModel.ts
+++ src/state/viewModel/rotorNavigationViewModel.ts
@@ -10,7 +10,7 @@ interface RotorState {
   rotor_value: string | null;
 }
 const initialState: RotorState = {
-  rotor_value: Constant.DATA_MODE
+  rotor_value: Constant.DATA_MODE,
 
 };
 const rotorNavigationSlice = createSlice({
